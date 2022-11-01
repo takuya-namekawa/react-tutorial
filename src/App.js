a7b26d1bb2ca35c79c77774215088983f2ee05fa
滑川卓弥 <tatatatatata89@gmail.com>
2022-11-01 14:04:55 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import "./styles.css";
+import { useState, useRef } from "react";
+import TodoList from "./TodoList";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo1", completed: false },
+  ]);
+
+  const todoNameRef = useRef(); //文字列を取得するにはuseRefを使用する要素を取得する事が出来る
+
+  const handleAddTodo = () => {
+    //タスクを追加する関数を記述する
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: "1", name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
+  const handleClear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  //refのフィールドを付けた所に同じ名前で当てる必要がある
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスクを追加</button>
+      <button onClick={handleClear}>完了したタスクの削除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </div>
   );
 }
